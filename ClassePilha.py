31e9a03b858c7da1cdfbdd1781dafa73f6535a83
Bianca Yuka <yuh.kokubu@gmail.com>
2025-01-29 02:16:19 -0300

    Update ClassePilha.py

--- ClassePilha.py
+++ ClassePilha.py
@@ -1,10 +1,8 @@
-
 class Pilha:
     def __init__(self, tam_max):
         self.elem = [None] * tam_max
         self.topo = -1 
         self.tam_max = tam_max
-
 
     def InicializaPilha(self):
         self.topo = -1
@@ -26,16 +24,12 @@ class Pilha:
 
     def Desempilha(self):
         if not self.PilhaVazia():
-            x = self.elem[self.topo]  # Pega o elemento do topo
-            self.elem[self.topo] = None  # Mantém o espaço vazio para reutilização
-            self.topo -= 1  # Atualiza o índice do topo
-
+            x = self.elem[self.topo] 
+            self.elem[self.topo] = None 
+            self.topo -= 1
             if self.PilhaVazia():
-                self.InicializaPilha()  # Reseta a pilha se necessário
-            
+                self.InicializaPilha()
             return x
-
-
         
     def mostrar_elementos(self):
         return self.elem
